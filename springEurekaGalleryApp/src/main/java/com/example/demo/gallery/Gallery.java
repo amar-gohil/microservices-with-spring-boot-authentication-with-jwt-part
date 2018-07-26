e09da5441808e2fee289f8017658344ee9202c13
amar-gohil <amar7gohil@gmail.com>
2018-07-26 15:47:24 +0530

    Add files via upload

---
+++ springEurekaGalleryApp/src/main/java/com/example/demo/gallery/Gallery.java
@@ -0,0 +1,23 @@
+package com.example.demo.gallery;
+
+import java.util.List;
+
+public class Gallery {
+
+	private int id;
+	private List images;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List getImages() {
+		return images;
+	}
+	public void setImages(List images) {
+		this.images = images;
+	}
+	
+	
+}
---
+++ springEurekaImageApp/src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Image;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	@Autowired
+	private Environment env;
+		
+	@RequestMapping("/images")
+	public List<Image> getImages() {
+		System.out.println("............................................................");
+		List<Image> images = Arrays.asList(
+			new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
+			new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
+			new Image(3, "The Last Traction Hero", "https://www.imdb.com/title/tt0096697/mediaviewer/rm1445594112"));
+		return images;
+	}
+	@RequestMapping("/")
+	public String home() {
+		// This is useful for debugging
+		// When having multiple instance of gallery service running at different ports.
+		// We load balance among them, and display which instance received the request.
+		return "Hello from images Service running at port: " + env.getProperty("local.server.port");
+	}
+}
